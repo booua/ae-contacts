6b356dc8ee7c0a75ac5055321244a772d2e33c59
Jacek Booua Paciorek <jack.paciorek@gmail.com>
2017-03-06 14:45:23 +0100

    style changes added gitignore

---
+++ js/index.js
@@ -0,0 +1,6 @@
+document.getElementById('search').onfocus = function() {
+  document.getElementById('search_icon').style.right = '16vw';
+}
+document.getElementById('search').onblur = function() {
+  document.getElementById('search_icon').style.right = '13vw';
+}
--- tmp/212be5cv1iskw.1d5u3dfzxhv26.php
+++
@@ -1,5 +0,0 @@
-<?php echo $this->render($templates . 'header.html',$this->mime,get_defined_vars(),0); ?>
-<?php echo $new_contact_data; ?>
-
-<?php echo $this->render($templates . 'dataForm.html',$this->mime,get_defined_vars(),0); ?>
-<?php echo $this->render($templates . 'footer.html',$this->mime,get_defined_vars(),0); ?>
